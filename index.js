cbafaba91cff408ff9026de082343124f68ac7ee
NguyenNguyen205 <92852929+NguyenNguyen205@users.noreply.github.com>
2023-05-30 16:10:44 +0700

    Add route

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({extended:true}))
 let token = process.env.TOKEN
-app.get('/',async (req, res) => {
+app.get("/", (req, res) => {
+    res.send("Vercel is working !!!")
+    return;
+})
+app.get('/detail',async (req, res) => {
     let config = {
         headers: {
             "Token": token,
@@ -22,7 +26,9 @@ app.get('/',async (req, res) => {
 
 })
 
+
 const port = process.env.PORT || 8080
+
 app.listen(port, () => {
     console.log(`Server is listening on ${port}`)
 })
